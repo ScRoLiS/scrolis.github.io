9cafc49626efe15b138a5ddc85a3f0632b59b7d3
ScRoLiS <34772324+ScRoLiS@users.noreply.github.com>
2020-09-19 02:45:39 +0300

    Add files via upload

---
+++ todo-list/precache-manifest.f30b21e9fcb011ca88548f74092eeb6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc647111dcc7e8e2e5de64cba533a1d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5c77e4c132d307f3137",
+    "url": "/static/css/main.cbdfc001.chunk.css"
+  },
+  {
+    "revision": "0c67fa66e9c9b5237aaf",
+    "url": "/static/js/2.caaa4eb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.caaa4eb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c77e4c132d307f3137",
+    "url": "/static/js/main.d44a8e7a.chunk.js"
+  },
+  {
+    "revision": "711714d7d018d95c9ba4",
+    "url": "/static/js/runtime-main.33471999.js"
+  }
+]);
